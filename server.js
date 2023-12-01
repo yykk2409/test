0b68ccae4356e0ab9305a2bf3964c0c8bb61c029
yykk2409 <maetaka-2022066@edu-g.gsn.ed.jp>
2023-12-01 12:40:45 +0900

    Google

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// 静的ファイルの提供
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ルートエンドポイントにGETリクエストを処理
+app.get('/test1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'imagetimer.html'));
+});
+app.get('/test2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'imagetimer2.html'));
+});
+// サーバーを指定のポートで起動
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
